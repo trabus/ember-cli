dfbc9d05ce1a50ef9c9ee289e3868fc5a5f9cd4d
trabus <trabus@gmail.com>
2014-08-19 14:15:48 -0700

    pod folder structure idea

---
+++ blueprints/pods/files/tests/unit/__type__/__name__-test.js
@@ -0,0 +1,12 @@
+import { test, moduleFor } from 'ember-qunit';
+
+moduleFor('<%= type %>:<%= dasherizedModuleName %>', '<%= classifiedModuleName %><%= capitalizedModuleType %>', {
+  // Specify the other units that are required for this test.
+  // needs: ['<%= type %>:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var controller = this.subject();
+  ok(controller);
+});
